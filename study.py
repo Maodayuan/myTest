0679c052410384f948652b72f583e755fa02ca43
毛达源 <maodayuan@MacBook-Pro.local>
2021-02-11 13:04:48 +0800

    添加文件

---
+++ study.py
@@ -0,0 +1,15 @@
+class Clang:
+    def __init__(self,name):
+        self.name = name
+    def setname(self,name):
+        self.name = name
+    def getname(self):
+        return self.name
+    def delname(self):
+        self.name='xxx'
+clang=Clang('python')
+print(clang.getname())
+clang.setname('dd')
+print(clang.getname())
+clang.delname()
+print(clang.getname())
